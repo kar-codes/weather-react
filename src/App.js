e625fd4510dcd2d7c8beb3dfa777f9e8b0812cdf
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-02-04 07:32:26 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import './App.css';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      Weather App
-    </div>
+      <div className="container">
+        <h1>Weather App</h1>
+          <footer>
+            This project was coded by <a href="https://kluik.netlify.app/" target="_blank">Kar</a> and is 
+            <a href="https://github.com/kar-codes/weatherAppDraft/blob/main/src/weatherStyle.css" target="_blank"> open-source </a>
+            on Github
+            </footer>
+          </div>
+        </div>
   );
 }
 
