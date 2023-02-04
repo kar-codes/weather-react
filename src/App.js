d9c4971cf41c890e6d45f3cc54c597b1df771e43
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-02-04 09:45:35 -0500

    overall CSS overhaul

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
           <footer>
-            This project was coded by <a href="https://kluik.netlify.app/" target="_blank" rel="noreferrer">Kar</a> and is 
+            Coded by <a href="https://kluik.netlify.app/" target="_blank" rel="noreferrer">Kar</a> and is 
             <a href="https://github.com/kar-codes/weatherAppDraft/blob/main/src/weatherStyle.css" target="_blank" rel="noreferrer"> open-source </a>
-            on Github
+            on Github.
             </footer>
           </div>
         </div>
