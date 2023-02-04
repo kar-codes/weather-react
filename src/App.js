71dcf5a754399bc04e5ada5f69a5af3e0efab488
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-02-04 07:53:42 -0500

    fixed broken packages

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <div className="container">
         <h1>Weather App</h1>
           <footer>
-            This project was coded by <a href="https://kluik.netlify.app/" target="_blank">Kar</a> and is 
-            <a href="https://github.com/kar-codes/weatherAppDraft/blob/main/src/weatherStyle.css" target="_blank"> open-source </a>
+            This project was coded by <a href="https://kluik.netlify.app/" target="_blank" rel="noreferrer">Kar</a> and is 
+            <a href="https://github.com/kar-codes/weatherAppDraft/blob/main/src/weatherStyle.css" target="_blank" rel="noreferrer"> open-source </a>
             on Github
             </footer>
           </div>
