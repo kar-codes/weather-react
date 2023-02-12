3b444584e15f9e6554dfc0c52dec1846a8111fa9
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-02-12 09:36:07 -0500

    API implementation

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import './Weather.css';
 
 export default function Weather(){
-    const apiKey = "f4b9b3c3f140t6ca1b114f1eo5df8045";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}';
-    return <div className="weather container">
+        const [ready, setReady] = useState(false);
+        const [weatherData, setWeatherData] = useState({});
+            function handleResponse(response){
+            console.log(response.data);
+            setWeatherData({
+                ready: true,
+                city:response.data.city ,
+                temperature: response.main.temperature.current,
+                time: response.data.time,
+                description: response.data.description ,
+                wind: response.data.speed,
+                humidity : response.data.temperature.humidity,
+                pressure: response.data.temperature.pressure,
+            })
+            setReady(true);
+
+    }
+
+    if (weatherData.ready){
+        return <div className="weather container">
         <form>
             <div className="row search-bar">
                 <div className="col-9">
@@ -16,28 +33,38 @@ export default function Weather(){
                 </div>
             </div>
         </form>
-       <h1>Tallinn</h1>
+       <h1 className="text-capitalize">{weatherData.city}</h1>
        <div className="row">
             <div className="col-12">
                 <div className="weather-main">
                     <div className="weather-current">
-                        <span className="temperature"> <strong>7</strong></span>
+                        <span className="temperature"> <strong>Math.round{weatherData.temperature}</strong></span>
                         <span className="unit mt-2"> <a href="">ºC</a> | <a href="">ºF</a> </span>
                     </div>
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny" />
+                    <img src={weatherData.icon_url} alt="Sunny" />
                     <ul className="p-0">
-                        <li>Sunny</li>
-                        <li>Saturday 14:08</li>
+                        <li className="text-capitalize">{weatherData.description}</li>
+                        <li>{weatherData.time}</li>
                     </ul>
                 </div>
             </div>
        </div>
        <div className="weather-extra"> 
             <ul>
-                <li>Precipitation: 0%</li>
-                <li>Humidity: 21%</li>
-                <li>Wind: 5 km/h</li>
+                <li>Humidity: {weatherData.humidity}</li>
+                <li>Wind: {weatherData.wind}</li>
             </ul>
         </div>
     </div>
+    } else {
+        const apiKey = "f4b9b3c3f140t6ca1b114f1eo5df8045";
+        let city = "london";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+
+    }
+
+   
+
+    
 }
