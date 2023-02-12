5d2251d36dadb52df833ca99d97b82617fb9ecf5
kar-codes <103388857+kar-codes@users.noreply.github.com>
2023-02-12 09:59:09 -0500

    updated API calls

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,23 +2,21 @@ import React, {useState} from "react";
 import axios from "axios";
 import './Weather.css';
 
-export default function Weather(){
-        const [ready, setReady] = useState(false);
-        const [weatherData, setWeatherData] = useState({});
+export default function Weather(props){
+        const [weatherData, setWeatherData] = useState({ ready: false});
             function handleResponse(response){
             console.log(response.data);
             setWeatherData({
                 ready: true,
                 city:response.data.city ,
-                temperature: response.main.temperature.current,
+                temperature: response.data.temperature.current,
+                icon: response.data.icon_url,
                 time: response.data.time,
-                description: response.data.description ,
-                wind: response.data.speed,
+                description: response.data.condition.description ,
+                wind: response.data.temperature.current.speed,
                 humidity : response.data.temperature.humidity,
-                pressure: response.data.temperature.pressure,
-            })
-            setReady(true);
-
+                pressure: response.data.temperature.pressure
+            });
     }
 
     if (weatherData.ready){
@@ -38,10 +36,10 @@ export default function Weather(){
             <div className="col-12">
                 <div className="weather-main">
                     <div className="weather-current">
-                        <span className="temperature"> <strong>Math.round{weatherData.temperature}</strong></span>
+                        <span className="temperature"> <strong>{Math.round(weatherData.temperature)}</strong></span>
                         <span className="unit mt-2"> <a href="">ºC</a> | <a href="">ºF</a> </span>
                     </div>
-                    <img src={weatherData.icon_url} alt="Sunny" />
+                    <img src={weatherData.icon} alt="Sunny" />
                     <ul className="p-0">
                         <li className="text-capitalize">{weatherData.description}</li>
                         <li>{weatherData.time}</li>
@@ -51,15 +49,14 @@ export default function Weather(){
        </div>
        <div className="weather-extra"> 
             <ul>
-                <li>Humidity: {weatherData.humidity}</li>
-                <li>Wind: {weatherData.wind}</li>
+                <li>Humidity: {weatherData.humidity} %</li>
+                <li>Wind: {weatherData.wind} km/h</li>
             </ul>
         </div>
     </div>
     } else {
         const apiKey = "f4b9b3c3f140t6ca1b114f1eo5df8045";
-        let city = "london";
-        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
 
     }
